5d700e263b18cd2a55a0012df34e6b33ec6637f4
EuSouBrunoGusmao <brunomgusmao@gmail.com>
2025-04-07 12:30:18 -0300

    Criei um outro workspace clone para testar o import

---
+++ src/exerciciosbasicos/Exercicio15TesteOtherWorkspaceClone.java
@@ -0,0 +1,12 @@
+package exerciciosbasicos;
+
+public class Exercicio15TesteOtherWorkspaceClone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		"Criei um outro workspace clone para testar o import"
+
+	}
+
+}
