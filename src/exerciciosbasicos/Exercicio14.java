8cb06622419f9d3484e03d40e430af3aa8cf4b51
EuSouBrunoGusmao <brunomgusmao@gmail.com>
2025-04-07 12:00:26 -0300

    de novo e de novo testando

---
+++ src/exerciciosbasicos/Exercicio14.java
@@ -0,0 +1,12 @@
+package exerciciosbasicos;
+
+public class Exercicio14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		"testando de novo e de novo"
+		
+	}
+
+}
